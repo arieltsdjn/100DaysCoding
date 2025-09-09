c620d750ccebaefd7569a085557754cd13a5cbb1
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-09-09 11:45:51 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Nama saya:");    
+        System.out.println(" Ariel Timotius Sudjana");  
+        System.out.printf("Umur saya %d tahun", 18);
+    }
+}
