7dc1da34a92669e1589b7916e9d7f59611aa2173
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-09-10 20:49:19 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // ini komentar satu baris
+        System.out.println("Contoh komentar satu baris");
+
+        /*
+         ini komentar 
+         lebih dari satu baris 
+        */
+        System.out.println("Contoh komentar lebih dari satu baris");
+    }
+}
