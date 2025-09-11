05687d3e549f466459a6bfc528fbc1ecbe290341
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-09-11 13:17:38 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        // \n -> baris baru
+        System.out.println("Halo\nDunia");  
+
+        // \t -> tab (spasi panjang)
+        System.out.println("Nama:\tAriel");  
+
+        // \b -> backspace (hapus karakter sebelumnya)
+        System.out.println("Belajarr\b Java");  
+
+    }
