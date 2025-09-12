aa6012e9975ebfda7b589879102aab2052e7b4da
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-09-12 23:09:30 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+        byte byte1 = 18;
+        short short1 = 44;
+        int int1 = 321;
+        long long1 = 12345;
+
+        System.out.println("Byte  : " + byte1);
+        System.out.println("Short : " + short1);
+        System.out.println("Int   : " + int1);
+        System.out.println("Long  : " + long1);
+    }
