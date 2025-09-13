35a2f149e4a155b69fe49743650bc53e2eb574b6
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-09-13 20:39:25 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        float phiFloat = 3.14f;
+        double phiDouble = 3.141592653589793;
+
+        System.out.println("Nilai float  : " + phiFloat);
+        System.out.println("Nilai double : " + phiDouble);
+    }
+}
