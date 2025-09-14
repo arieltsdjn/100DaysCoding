3be23a790ec89b3e7dadc4852d2c0ac9241f4450
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-09-14 19:53:13 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    char huruf = 'A';
+    boolean benar = true;
+    System.out.println("Char: " + huruf + ", Boolean: " + benar);
+  }
+}
