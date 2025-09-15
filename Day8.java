41665e792de08ede5fec599aef380ed8df03aca5
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-09-15 21:34:08 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,8 @@
+public class main {
+  public static void main(String[] args) {
+    String teks = "kenapa sih harus hello world?\n"
+                + "padahal banyak teks lain wkwk";
+    
+    System.out.println(teks);
+  }
+}
