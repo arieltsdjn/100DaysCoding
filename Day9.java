5752a0eabe1554d8c40d608d854522086d9ba3f6
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-09-16 22:11:46 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,8 @@
+public class main {
+  public static void main(String[] args) {
+    int x;          // ini toh deklarasi
+    x = 5;          // baru ini inisialisasi
+    x = x + 3;      // ya ini update
+    System.out.println(x);
+  }
+}
