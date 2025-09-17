3b954debf1c8be9c5bd10f5cb458deaf58868dca
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-09-17 21:23:55 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        final byte umur = 20;
+        final short tinggiBadan = 170;
+        final int uangSaku = 50000;
+        final long nomorInduk = 1234567890L;
+        final float beratBadan = 55.5f;
+        final double pi = 3.14159;
+        final char nilaiUjian = 'B';
+        final boolean lagiLapar = true;
+
+        System.out.println("Umur (byte): " + umur);
+        System.out.println("Tinggi badan (short): " + tinggiBadan);
+        System.out.println("Uang saku (int): " + uangSaku);
+        System.out.println("Nomor induk (long): " + nomorInduk);
+        System.out.println("Berat badan (float): " + beratBadan);
+        System.out.println("Pi (double): " + pi);
+        System.out.println("Nilai ujian (char): " + nilaiUjian);
+        System.out.println("Lagi lapar? (boolean): " + lagiLapar);
+    }
+}
