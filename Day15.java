a539fb44eb66183787a2d5449e2645ed67f1b3fb
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-09-22 18:27:04 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 5;
+        int c = 10;
+        int d = 3;
+
+        int tambah = a + b;
+        int kurang = c - d;
+        System.out.println("Hasil penjumlahan: " + tambah);
+        System.out.println("Hasil pengurangan: " + kurang);
+        
+    }
+}
+    
+/*
+atau bisa langsung kek gini:
+System.out.println("7 + 5 = " + (7 + 5));
+System.out.println("10 - 4 = " + (10 - 4));
+*/
