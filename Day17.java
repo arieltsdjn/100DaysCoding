3374b34701cbd50aca7245c616d628ca1933fa78
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-09-24 21:13:43 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        // operasi modulus
+        int hasil = a % b;
+
+        // output
+        System.out.println(a + " % " + b + " = " + hasil);
+
+        // contoh lain
+        System.out.println("15 % 4 = " + (15 % 4));
+        System.out.println("8 % 2 = " + (8 % 2));
+    }
+}
