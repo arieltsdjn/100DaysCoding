a7899bf05c9f5ac9c95a354470f4051052049430
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-09-25 20:32:50 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        byte a = 10;
+        int b = a;       
+        long c = b;       
+        float d = c;      
+        double e = d;     
+
+        System.out.println("byte: " + a);
+        System.out.println("int: " + b);
+        System.out.println("long: " + c);
+        System.out.println("float: " + d);
+        System.out.println("double: " + e);
+    }
+}
