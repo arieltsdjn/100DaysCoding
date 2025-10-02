f0312e072b1c6bb46e4e37f9f532ccfc1c2aeb52
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-10-02 20:12:50 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+        
+        int a = 3;
+        System.out.println(++a);
+        System.out.println(a);
+
+        int y = 5;
+        System.out.println(y--);
+        System.out.println(y);
+        
+        int b = 4;
+        System.out.println(--b);
+        System.out.println(b);
+    }
+}
