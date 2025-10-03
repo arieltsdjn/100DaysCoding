d368cdf02016332c23c9c6bd2f29eef4192f18d4
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-10-03 21:44:12 +0800

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("a = " + a);
+
+        int b = 10;
+        b += 5;
+        System.out.println("b += 5 → " + b);
+
+        int c = 10;
+        c -= 3;
+        System.out.println("c -= 3 → " + c);
+
+        int d = 10;
+        d *= 2;
+        System.out.println("d *= 2 → " + d);
+
+        int e = 20;
+        e /= 4;
+        System.out.println("e /= 4 → " + e);
+
+        int f = 17;
+        f %= 5;
+        System.out.println("f %= 5 → " + f);
+    }
+}
