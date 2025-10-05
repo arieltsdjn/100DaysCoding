9350102562d6ba85bcbbf7a1e15d5b9121902c2b
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-10-05 23:15:40 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("\n=== Hasil Perbandingan ===");
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+    }
+}
