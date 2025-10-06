fa82510658aa25b4c13bcf89e45c9736d4942736
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-10-06 20:13:14 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 12;
+
+        System.out.println(a < b);  
+        System.out.println(a > b); 
+
+        boolean hasil1 = a < b;
+        boolean hasil2 = a > b;
+
+        System.out.println("a < b = " + hasil1);
+        System.out.println("a > b = " + hasil2);
+    }
+}
