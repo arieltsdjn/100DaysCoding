7c3d19f9428568d850553f0813450d768234b022
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-10-07 22:02:08 +0800

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 12;
+        int c = 8
+
+        System.out.println(a <= b);  
+        System.out.println(a >= b);
+        System.out.println(a >= c); 
+
+        boolean hasil1 = a <= b;
+        boolean hasil2 = a >= b;
+        boolean hasil3 = a >= c;
+
+        System.out.println("a <= b = " + hasil1);
+        System.out.println("a >= b = " + hasil2);
+        System.out.println("a >= c = " + hasil3);
+    }
+}
