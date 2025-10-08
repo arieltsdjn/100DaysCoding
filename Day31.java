ff2810e3ce296b1b75e802f58053c9780885b2ed
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-10-08 21:14:51 +0800

    Create Day31.java

---
+++ Day31.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int umur = 18;
+        boolean punyaSIM = true;
+
+        System.out.println(umur >= 17 && punyaSIM);
+    }
+}
