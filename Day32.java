62453fff367c714cb56647e3ae48a73adfab0e7b
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-10-09 23:09:56 +0800

    Create Day32.java

---
+++ Day32.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean anu = true;
+        boolean anu2 = false;
+
+        System.out.println(anu || anu2);
+        System.out.println(anu2 || anu2);
+    }
+}
