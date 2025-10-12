e61ac6ea84587def6a271788d53698cfc36f965a
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-10-12 21:46:35 +0800

    Update Day35.java

--- Day35.java
+++ Day35.java
@@ -5,13 +5,13 @@ public class Main {
         int angka = i.nextInt();
         
         if (angka >= 20) {
-            System.out.println(angka + " adalah puluhan");
+            System.out.println(angka + " adalah puluhan, dan seterusnya..");
         } else if (angka >= 10) {
             System.out.println(angka + " adalah belasan");
         } else if (angka > 0){
             System.out.println(angka + " adalah satuan");
         } else {
-            System.out.println(angka + " bukan satuan dan puluhan");
+            System.out.println(angka + " bukan satuan, belasan ataupun puluhan");
         }
         
     }
