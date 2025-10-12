2d7fe01e1024ad826c82546234a5efb906bafd59
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-10-12 21:40:50 +0800

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+        int angka = i.nextInt();
+        
+        if (angka >= 20) {
+            System.out.println(angka + " adalah puluhan");
+        } else if (angka >= 10) {
+            System.out.println(angka + " adalah belasan");
+        } else if (angka > 0){
+            System.out.println(angka + " adalah satuan");
+        } else {
+            System.out.println(angka + " bukan satuan dan puluhan");
+        }
+        
+    }
+}
