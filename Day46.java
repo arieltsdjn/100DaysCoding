4240b226878d11762d1e5c8eb3a2eb9db7e1e0af
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-10-23 21:24:00 +0800

    Create Day46.java

---
+++ Day46.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Menu {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("====== PASAR GELAP ======\nmenu yang tersedia:\n1. narkotika\n2. ganja\n3. AK-47\n4. granat\n5. sianida\n=========================\nmau yang mana? pilih satu aja:  ");
+        int menu = s.nextInt();
+        String pilih = switch (menu) {
+            case 1 ->
+                "narkotika";
+            case 2 ->
+                "ganja";
+            case 3 ->
+                "AK-47";
+            case 4 ->
+                "granat";
+            case 5 ->
+                "sianida";
+            default ->
+                "pilihan tidak valid";
+        };
+        System.out.println("\nanda memilih " + pilih + ".\nselanjutnya, silahkan atur jadwal & lokasi pertemuan untuk diskusi harga dan mengambil pesanan anda.\ncp. 081234567890");
+    }
+}
