9b5438ff4a1ce07d97a54f112eb4cac50c7dbc1e
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-10-29 22:24:14 +0800

    Create Day52.java

---
+++ Day52.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int angka = 1;
+        while (angka <= 5) {
+            System.out.println(angka);
+            angka++;
+        }
+    }
+}
