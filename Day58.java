808f0d19c27796a478132cb5302b9ff265fb22a6
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-11-04 21:53:56 +0800

    Add Main class with counting loops

---
+++ Day58.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int angka = 10;
+        for (int i = 1; i <= angka; i++) {
+            System.out.println(i);
+        }
+        for (int ij = angka; ij >= 1; ij--) {
+            System.out.println(ij);
+        }
+    }
+}
