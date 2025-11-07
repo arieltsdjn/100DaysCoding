e0ea0bbe6a6435db6f208d03c728bb7d3e14e6e0
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-11-07 23:13:22 +0800

    Create Day61.java

---
+++ Day61.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int m = 5;
+        int n = 20;
+        for (int i = 1; i <= n; i++) {
+            if (i%m==0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
