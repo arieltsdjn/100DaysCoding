a30f82c135f19a917652659213deca262268bf09
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-11-09 21:47:27 +0800

    Update Day63.java

--- Day63.java
+++ Day63.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int n = 10;
-        int total = 0;
+        int total = 1;
         for (int i = 1; i <= n; i++) {
             System.out.println(i);
             total*=i;
