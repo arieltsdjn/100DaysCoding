897ca82e4e24b7b40f6a5a7cc41f0d44c63bd124
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-11-16 23:32:53 +0800

    Create Day70.java

---
+++ Day70.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int p = 10;
+        int l = 4;
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < p; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
