258c9f0c969cacee9ddd6c84f8df207ce5d9661f
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-11-21 19:56:27 +0800

    Create Day75.java

---
+++ Day75.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String teks ="Belajar Java";
+        System.out.println("teks: "+teks);
+        System.out.println("panjang teks: "+teks.length());
+        System.out.println("uppercase: "+teks.toUpperCase());
+        System.out.println("lowercase: "+teks.toLowerCase());
+    }
+}
