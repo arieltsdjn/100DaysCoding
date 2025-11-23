dd6e9c234f527cc51ebed151c7c55fec68803b8c
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-11-23 22:18:52 +0800

    Create Day77.java

---
+++ Day77.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String teks = "   Aku Suka Java   ";
+        String trim = text.trim();
+        System.out.println(trimmed);
+        String sub = trim.substring(6);
+        System.out.println(sub);
+        String replace = sub.replace("Java", "Jawa");
+        System.out.println(replace);
+    }
+}
