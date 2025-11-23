49675066e6d594d348f6b3b9870bae90d6ae2af2
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-11-23 22:20:40 +0800

    Update Day77.java

--- Day77.java
+++ Day77.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         String teks = "   Aku Suka Java   ";
-        String trim = text.trim();
-        System.out.println(trimmed);
-        String sub = trim.substring(6);
+        String trim = teks.trim();
+        System.out.println(trim);
+        String sub = trim.substring(4);
         System.out.println(sub);
         String replace = sub.replace("Java", "Jawa");
         System.out.println(replace);
