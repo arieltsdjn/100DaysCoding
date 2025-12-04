4412639efea7ac552cc620ea1a619fafbb005bf3
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-12-04 18:07:29 +0800

    Create Day88.java

---
+++ Day88.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+            int[] angka = {5, 9, 3, 7};
+
+            for (int a : angka) {
+            System.out.println(a);
+            }
+        }
+    }
+}
