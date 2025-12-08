7853d7b85a22f50a4822bddf18d7a1f5a7e8277d
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-12-08 17:51:54 +0800

    Create Day92.java

---
+++ Day92.java
@@ -0,0 +1,8 @@
+public class Main {
+    static double luas(double p, double l){
+        return p*l;
+    }
+    public static void main(String[] args) {
+        System.out.println(luas(2.5,3));
+    }
+}
