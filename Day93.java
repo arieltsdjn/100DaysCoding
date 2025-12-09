50ad8952c8de6d4214667e4d38cfd00fb16e0c32
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-12-09 20:04:46 +0800

    Create Day93.java

---
+++ Day93.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double anu = 4.7;
+        System.out.println("ceil: " + Math.ceil(x));
+        System.out.println("floor: " + Math.floor(x));
+        System.out.println("round: " + Math.round(x));
+    }
+}
