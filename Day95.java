d792dc869db1fb7c494c65a556b0c03569ae6a5b
Ariel Timotius Sudjana <100550171+arieltsdjn@users.noreply.github.com>
2025-12-11 20:18:59 +0800

    Create Day95.java

---
+++ Day95.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void cetakNama(String nama) {
+        System.out.println("halo, " + nama);
+    }
+    public static void main(String[] args) {
+        cetakNama("Ariel");
+    }
+}
